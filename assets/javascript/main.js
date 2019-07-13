ba24e1eb4328764b75b0e2f8541991ceef1e1228
Benjamin Morrow <morrowbenjamin42@gmail.com>
2019-07-13 11:19:36 -0400

    initial js work

---
+++ assets/javascript/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    // "&begin_date=" + startYear + "&end_date=" + endYear +
+    var apiKey = "N7TjPA0Be6WfOQzEgqi6pu5mJpAsgXe7";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=" + apiKey + "&fl=" + numRecordsChosen;
+    var searchTerm = "New York Times";
+    var numRecords = ["1", "5", "10"];
+    var numRecordsChosen = numRecords[0];
+    var startYear = "2018";
+    var endYear = "2019";
+
+    $.ajax({
+        url : queryURL,
+        method : 'GET'
+    }).then(function(response){
+        console.log(response);
+    });
+
+
+});
