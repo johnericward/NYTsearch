6df7ad02cfd62bd4f03f1e9c0a80cc593f871743
Benjamin Morrow <morrowbenjamin42@gmail.com>
2019-07-13 12:05:51 -0400

    more js work

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,18 +1,26 @@
 $(document).ready(function(){
     // "&begin_date=" + startYear + "&end_date=" + endYear +
     var apiKey = "N7TjPA0Be6WfOQzEgqi6pu5mJpAsgXe7";
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=" + apiKey + "&fl=" + numRecordsChosen;
-    var searchTerm = "New York Times";
-    var numRecords = ["1", "5", "10"];
-    var numRecordsChosen = numRecords[0];
-    var startYear = "2018";
-    var endYear = "2019";
-
+    var searchTerm = "Obama";
+    var articleAmount = "10";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=" + apiKey;
+    
     $.ajax({
-        url : "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=george+washingtons&api-key=N7TjPA0Be6WfOQzEgqi6pu5mJpAsgXe7",
+        url : queryURL,
         method : 'GET'
     }).then(function(response){
         console.log(response);
+        for(var i = 0; i < 10; i++)
+        {
+            var headlines = response.response.docs[i].headline.main;
+            var author = response.response.docs[i].byline.original;
+            console.log(headlines);
+            console.log(author);
+
+            $("#benResults").append($("<div>",{"id" : "article"}));
+            $("#article").append($("<p>",{"text" : headlines, "id" : "headline" + i}));
+            $("#headline" + i).append($("<p>",{"text" : author}));
+        }
     });
 
 
